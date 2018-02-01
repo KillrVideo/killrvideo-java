152a4a627d5cfee6590cbebc3b4d1f930e32e749
Brian T. Smith <bsmith@systemfabricworks.com>
2018-01-31 18:36:58 -0600

    Use class-path jars in run.sh to define classpath. It works on linux!

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 export KILLRVIDEO_DOCKER_IP
 export KILLRVIDEO_HOST_IP
 # use everything under ~/.m2 ... good luck
-java -cp target/classes:`find /home/bsmith/.m2/ -name \*.jar|grep -v guava-10| grep -v guava-18|tr '\n' ':'` killrvideo.KillrVideoServer |& tee killrvideo.out
+java -cp target/classes:`cat classpath-jars| sed "s@^@$HOME/@g"| tr '\n' ':'` killrvideo.KillrVideoServer |& tee killrvideo.out
